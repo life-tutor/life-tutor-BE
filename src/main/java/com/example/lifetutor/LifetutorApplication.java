64968abe26ead3df1b0450041d8d5294b6e8843e
MoonDoorKing <rizingsniper@kakao.com>
2022-08-09 09:57:19 +0900

    환경설정

---
+++ src/main/java/com/example/lifetutor/LifetutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifetutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifetutorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifetutor/LifetutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifetutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifetutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
