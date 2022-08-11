5ffc6b81fb735fc7f9907ad1f7cbd633369f5bfd
ju-ei8ht <qkr8125@gmail.com>
2022-08-11 11:56:37 +0900

    MethodArgumentNotValidException 추가(commentRequestDto의 content notBlank validate exception)

--- src/main/java/com/example/lifetutor/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/lifetutor/config/GlobalExceptionHandler.java
@@ -2,12 +2,22 @@ package com.example.lifetutor.config;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handlerException(IllegalArgumentException e){
         String msg = e.getMessage();
+        return new ResponseEntity<>(
+                msg, HttpStatus.BAD_REQUEST
+        );
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handlerException(MethodArgumentNotValidException e){
+        String msg = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         return new ResponseEntity<>(
                 msg, HttpStatus.BAD_REQUEST
         );
