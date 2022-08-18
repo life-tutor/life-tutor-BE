2b4ad95ebe4c1725390a382e5b2341a9b5f492af
fox9d <devafox62@gmail.com>
2022-08-18 20:23:07 +0900

    좋아요를 한 게시글 체크

--- src/main/java/com/example/lifetutor/post/dto/response/Content.java
+++ src/main/java/com/example/lifetutor/post/dto/response/Content.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -21,15 +20,15 @@ public class Content {
     private boolean isLike;
     private List<String> hashtag;
 
-    public Content(Long postingId, String nickname, String title, LocalDate date, String posting_content, boolean b, List<String> hashtag, int comment_count, int like_count) {
+    public Content(Long postingId, String nickname, String title, LocalDate date, String posting_content, List<String> hashtag, int comment_count, int like_count, boolean isLike) {
         this.posting_id = postingId;
         this.nickname = nickname;
         this.title = title;
         this.date = date;
         this.posting_content = posting_content;
-        this.isLike = b;
         this.hashtag = hashtag;
         this.comment_count = comment_count;
         this.like_count = like_count;
+        this.isLike = isLike;
     }
 }
