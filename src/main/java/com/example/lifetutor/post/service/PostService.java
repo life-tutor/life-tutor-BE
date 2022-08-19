14ec1fa18eed22cb3e3e2dcc251a18bd2952f96e
fox9d <devafox62@gmail.com>
2022-08-19 18:11:42 +0900

    PostHashtag 구현

--- src/main/java/com/example/lifetutor/post/service/PostService.java
+++ src/main/java/com/example/lifetutor/post/service/PostService.java
@@ -2,7 +2,9 @@ package com.example.lifetutor.post.service;
 
 import com.example.lifetutor.config.security.UserDetailsImpl;
 import com.example.lifetutor.hashtag.model.Hashtag;
+import com.example.lifetutor.hashtag.model.PostHashtag;
 import com.example.lifetutor.hashtag.repository.HashtagRepository;
+import com.example.lifetutor.hashtag.repository.PostHashtagRepository;
 import com.example.lifetutor.likes.model.Likes;
 import com.example.lifetutor.post.dto.request.PostRequestDto;
 import com.example.lifetutor.post.dto.response.Content;
@@ -10,6 +12,7 @@ import com.example.lifetutor.post.dto.response.PostResponseDto;
 import com.example.lifetutor.post.model.Post;
 import com.example.lifetutor.post.repository.PostRepository;
 import com.example.lifetutor.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -20,16 +23,20 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
 
     private final PostRepository postRepository;
     private final HashtagRepository hashtagRepository;
+    private final PostHashtagRepository postHashtagRepository;
 
-    public PostService(PostRepository postRepository, HashtagRepository hashtagRepository) {
+    @Autowired
+    public PostService(PostRepository postRepository, HashtagRepository hashtagRepository, PostHashtagRepository postHashtagRepository) {
         this.postRepository = postRepository;
         this.hashtagRepository = hashtagRepository;
+        this.postHashtagRepository = postHashtagRepository;
     }
 
     @Transactional
@@ -49,7 +56,7 @@ public class PostService {
             String title = p.getTitle();
             LocalDateTime date = p.getDate();
             String posting_content = p.getPosting_content();
-            List<Hashtag> hashtag = hashtagRepository.findAllByPostId(p.getId());
+            List<PostHashtag> postHashtags = postHashtagRepository.findAllByPostId(p.getId());
             int comment_count = p.getComments().size();
             int like_count = p.getLikes().size();
             boolean isLike = false;
@@ -63,13 +70,19 @@ public class PostService {
             }
 
             List<String> hashtags = new ArrayList<>();
-            for (Hashtag h : hashtag) hashtags.add(h.getHashtag());
+
+            for (PostHashtag h : postHashtags) {
+                Hashtag ht = hashtagRepository.findById(h.getHashtag().getId())
+                        .orElseThrow(() -> new IllegalArgumentException("No search date"));
+                hashtags.add(ht.getHashtag());
+            }
 
             Content c = new Content(postingId, nickname, title, date, posting_content, hashtags, comment_count, like_count, isLike);
             content.add(c);
         }
         return new PostResponseDto(content, posts.isLast());
     }
+
     @Transactional
     public void registerPost(PostRequestDto postRequestDto, UserDetailsImpl userDetails) {
         String title = postRequestDto.getTitle();
@@ -82,8 +95,17 @@ public class PostService {
         Post post = new Post(user, title, posting_content);
 
         for (String s : hashtag) {
-            Hashtag ht = new Hashtag(post, s);
-            hashtagRepository.save(ht);
+            Hashtag ht = hashtagRepository.findByHashtag(s);
+            if (ht == null) {
+                Hashtag enrollmentHt = new Hashtag(s);
+                PostHashtag postHashtag = new PostHashtag(post, enrollmentHt);
+                hashtagRepository.save(enrollmentHt);
+                postHashtagRepository.save(postHashtag);
+            } else {
+                PostHashtag postHashtag = new PostHashtag(post, ht);
+                postHashtagRepository.save(postHashtag);
+            }
+
         }
 
         postRepository.save(post);
