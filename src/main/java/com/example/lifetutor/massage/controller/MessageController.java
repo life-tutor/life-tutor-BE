23f8ca254a29297e07d676e3e41c3c7b435963af
ju-ei8ht <qkr8125@gmail.com>
2022-08-24 19:15:33 +0900

    message 보낼 때 headerToken 검사 X message service slf4j log 지움

--- src/main/java/com/example/lifetutor/massage/controller/MessageController.java
+++ src/main/java/com/example/lifetutor/massage/controller/MessageController.java
@@ -2,17 +2,21 @@ package com.example.lifetutor.massage.controller;
 
 import com.example.lifetutor.massage.dto.request.MessageRequestDto;
 import com.example.lifetutor.massage.service.MessageService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class MessageController {
 
     private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     @MessageMapping("/{room_id}")
     public void message(@RequestBody MessageRequestDto message, @DestinationVariable Long room_id){
