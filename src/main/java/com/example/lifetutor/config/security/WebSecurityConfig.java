5c5550578c7a635fbea5f289688e0bb320b5c8f5
MoonDoorKing <rizingsniper@kakao.com>
2022-08-31 01:03:35 +0900

    CODE MODIFY SECERY KEY 수정

--- src/main/java/com/example/lifetutor/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/lifetutor/config/security/WebSecurityConfig.java
@@ -245,7 +245,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("http://togetherheyyo.s3-website.ap-northeast-2.amazonaws.com/");
+        configuration.addAllowedOrigin("https://it-ing.co.kr");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
