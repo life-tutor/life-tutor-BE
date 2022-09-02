ae43fe90459809e5bf1544b039056f22016770e0
ju-ei8ht <qkr8125@gmail.com>
2022-09-02 21:39:32 +0900

    CI/CD 테스트 실패 다시 merge

--- src/main/java/com/example/lifetutor/massage/service/MessageService.java
+++ src/main/java/com/example/lifetutor/massage/service/MessageService.java
@@ -22,7 +22,7 @@ public class MessageService {
     // 채팅 메시지
     public void message(MessageRequestDto message, Long room_id){
         String nickname = message.getNickname();
-        if(!nickname.isEmpty() && MessageRequestDto.Enter.ENTER.equals(message.getEnter())){
+        if(!nickname.isEmpty()&&MessageRequestDto.Enter.ENTER.equals(message.getEnter())){
             message.setMessage(nickname + "님이 들어왔습니다.");
         }
         String time = LocalTime.now().format(DateTimeFormatter.ofPattern("a hh:mm").withLocale(Locale.KOREA));
