a2914ee3754d6d57b927b5a632403807d5d83466
ju-ei8ht <qkr8125@gmail.com>
2022-09-07 00:04:29 +0900

    message service x -> controller

--- src/main/java/com/example/lifetutor/massage/controller/MessageController.java
+++ src/main/java/com/example/lifetutor/massage/controller/MessageController.java
@@ -2,28 +2,30 @@ package com.example.lifetutor.massage.controller;
 
 import com.example.lifetutor.massage.dto.request.MessageRequestDto;
 import com.example.lifetutor.massage.dto.response.MessageResponseDto;
-import com.example.lifetutor.massage.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 @RestController
 public class MessageController {
-
-    private final MessageService messageService;
     private final SimpMessageSendingOperations sendingOperations;
-
     @Autowired
-    public MessageController(MessageService messageService, SimpMessageSendingOperations sendingOperations) {
-        this.messageService = messageService;
+    public MessageController(SimpMessageSendingOperations sendingOperations) {
         this.sendingOperations = sendingOperations;
     }
 
     @MessageMapping("/{room_id}")
     public void message(MessageRequestDto message, @DestinationVariable Long room_id){
-        MessageResponseDto result = messageService.message(message, room_id);
+        String nickname = message.getNickname();
+        if(MessageRequestDto.Enter.ENTER.equals(message.getEnter())) message.setMessage(nickname + "님이 들어왔습니다.");
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("a hh:mm").withLocale(Locale.KOREA));
+        MessageResponseDto result = new MessageResponseDto(time,message);
         sendingOperations.convertAndSend("/api/sub/" + room_id, result);
     }
 }
