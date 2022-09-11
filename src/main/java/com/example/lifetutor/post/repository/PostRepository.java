80045fbf0d853c254314dd70b9a8cd364b5aa107
fox9d <devafox62@gmail.com>
2022-09-11 14:22:05 +0900

    feature : 게시글 해시태그 검색어 추천기능 구현

--- src/main/java/com/example/lifetutor/post/repository/PostRepository.java
+++ src/main/java/com/example/lifetutor/post/repository/PostRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -18,6 +19,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p WHERE p.id in :idArr")
     Page<Post> findAll(Pageable pageable, Long[] idArr);
+
+    @Query("select distinct p from Post p inner join p.hashtags h where h.hashtag.hashtag=:hashtag order by p.id desc ")
+    Page<Post> postByHashtag(Pageable pageable,  @Param("hashtag") String hashtag);
 
 //    @Query("SELECT p FROM Post p WHERE p.hashtags IN :hashtags")
 //    Page<Post> findAllOrderById(Pageable pageable, List<Hashtag> hashtags);
