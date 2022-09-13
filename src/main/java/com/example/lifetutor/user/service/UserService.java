2861c20eace783694f349672926ba0a348200b38
MoonDoorKing <rizingsniper@kakao.com>
2022-09-13 10:21:26 +0900

    리프레시토큰 추가

--- src/main/java/com/example/lifetutor/user/service/UserService.java
+++ src/main/java/com/example/lifetutor/user/service/UserService.java
@@ -1,6 +1,10 @@
 package com.example.lifetutor.user.service;
 
+import com.example.lifetutor.comment.model.Comment;
 import com.example.lifetutor.comment.repository.CommentRepository;
+import com.example.lifetutor.config.security.jwt.HeaderTokenExtractor;
+import com.example.lifetutor.config.security.jwt.JwtDecoder;
+import com.example.lifetutor.config.security.jwt.JwtTokenUtils;
 import com.example.lifetutor.hashtag.model.Hashtag;
 import com.example.lifetutor.hashtag.model.PostHashtag;
 import com.example.lifetutor.hashtag.repository.HashtagRepository;
@@ -13,8 +17,10 @@ import com.example.lifetutor.user.dto.request.UpdateMyPasswordRequestDto;
 import com.example.lifetutor.user.dto.response.ContentResponseDto;
 import com.example.lifetutor.user.dto.response.MyPageResponseDto;
 import com.example.lifetutor.user.dto.response.ShowMyPostsResponseDto;
+import com.example.lifetutor.user.model.Auth;
 import com.example.lifetutor.user.model.Role;
 import com.example.lifetutor.user.model.User;
+import com.example.lifetutor.user.repositroy.AuthRepository;
 import com.example.lifetutor.user.repositroy.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,11 +32,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -41,6 +49,9 @@ public class UserService {
     private final HashtagRepository hashtagRepository;
     private final PostHashtagRepository postHashtagRepository;
     private final CommentRepository commentRepository;
+    private final AuthRepository authRepository;
+    private final JwtDecoder jwtDecoder;
+    private final HeaderTokenExtractor extractor;
     //회원가입
     public ResponseEntity<?> registerUser(SignupRequestDto signupRequestDto) {
         String username = signupRequestDto.getUsername();
@@ -110,6 +121,40 @@ public class UserService {
         return new ResponseEntity<>(showMyPostsResponseDto, HttpStatus.valueOf(200));
     }
 
+    public ResponseEntity<ShowMyPostsResponseDto> showMyCommentInPost(int page, int size, User user) {
+        Sort.Direction direction = Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, "date");
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Comment> comments = commentRepository.findAllByUser(pageable,user);
+
+        List<Comment> commentList = comments.getContent();
+
+        List<ContentResponseDto> contentResponseDtos = new ArrayList<>();
+        for(int i = 0 ; i < commentList.size(); i++) {
+            Long postingId = commentList.get(i).getPost().getId();
+            String nickname = commentList.get(i).getPost().getUser().getNickname();
+            String title = commentList.get(i).getPost().getTitle();
+            LocalDateTime date = commentList.get(i).getDate();
+            String content = commentList.get(i).getPost().getPosting_content();
+            List<PostHashtag> hashtag = postHashtagRepository.findAllByPostId(commentList.get(i).getPost().getId());
+            int comment_count = commentList.get(i).getPost().getComments().size();
+            int like_count = commentList.get(i).getLikes().size();
+
+            List<String> hashtags = new ArrayList<>();
+
+            for (PostHashtag postHashtag : hashtag) {
+                Hashtag hashtag1 = hashtagRepository.findById(postHashtag.getHashtag().getId()).get();
+                hashtags.add(hashtag1.getHashtag());
+            }
+
+            ContentResponseDto contentResponseDto = new ContentResponseDto(postingId, nickname, title, date, content, hashtags, comment_count, like_count);
+            contentResponseDtos.add(contentResponseDto);
+        }
+        ShowMyPostsResponseDto showMyPostsResponseDto = new ShowMyPostsResponseDto(contentResponseDtos,comments.isLast());
+        return new ResponseEntity<>(showMyPostsResponseDto, HttpStatus.valueOf(200));
+    }
+
     public void checkEmail(String username) {
         if (userRepository.existsByUsername(username))
             throw new IllegalArgumentException("중복된 이메일 입니다.");
@@ -154,6 +199,33 @@ public class UserService {
         userRepository.save(user);
 
         return new ResponseEntity<>("변경이 완료 되었습니다.", HttpStatus.valueOf(200));
+    }
+    @Transactional
+    public ResponseEntity<?> reIssueRefreshToken(HttpServletRequest request) {
+        String accessToken = extractor.extract(request.getHeader("Authorization"), request);
+
+        String refreshToken = request.getHeader("RefreshToken");
+
+        String username = jwtDecoder.decodeUserToken(accessToken);
+
+        Optional<Auth> findUserRefreshToken = authRepository.findByUsername(username);
+        String token = null;
+        if (findUserRefreshToken.isPresent()) {
+            if (findUserRefreshToken.get().getRefreshToken().equals(refreshToken)) {
+                if (jwtDecoder.isExpiredToken(refreshToken)) {
+                    return new ResponseEntity<>("만료된 리프레쉬 토큰입니다.", HttpStatus.valueOf(401));
+                } else {
+                    // 새로운 엑세스 토큰 발급
+                    token = JwtTokenUtils.generateJwtToken(username);
+                    // 접속 유저의 엑세스 토큰 업데이트
+                }
+            } else {
+                return new ResponseEntity<>("저장된 리프레쉬 토큰과 일치하지 않습니다.", HttpStatus.valueOf(401));
+            }
+        } else {
+            return new ResponseEntity<>("존재하지 않는 리프레쉬 토큰입니다.", HttpStatus.valueOf(401));
+        }
+        return ResponseEntity.ok().header("Authorization","BEARER " + token).body("토큰 재발급");
     }
 
 }
